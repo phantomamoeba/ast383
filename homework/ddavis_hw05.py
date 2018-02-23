0f2be3309e348c714a2d2c0584cf0b663f4fbf8a
phantomamoeba <polonius@davistx.com>
2018-02-23 12:35:49 -0600

    hw05

--- homework/ddavis_hw05.py
+++ homework/ddavis_hw05.py
@@ -14,7 +14,7 @@ poisson_avg_value_per_photon = 0.1
 def pix_dist(x1,y1,x2,y2):
     return np.sqrt((x1-x2)**2 + (y1-y2)**2)
 
-def make_true_image(dim_x, dim_y, num_points):
+def make_image(dim_x, dim_y, num_points):
     # force no adjacent point sources
     #todo: sanity check num_points vs dim_x, y s|t the configuration is possible
     #a bit simplistic, but sufficient and safe (each point claims the 3x3 elements centered on it)
@@ -25,7 +25,7 @@ def make_true_image(dim_x, dim_y, num_points):
 
     #todo: if relax the restriction on dimensions and num of points, could add a safety here
     #todo: to detect no-solution cases and restart
-    for i in range(num_points):
+    for _ in range(num_points):
         x = np.random.choice(range(dim_x))
         y = np.random.choice(range(dim_y))
 
@@ -45,76 +45,142 @@ def make_true_image(dim_x, dim_y, num_points):
 def make_psf(dim_x, dim_y):
     psf = np.random.random((dim_x, dim_y))
     psf /= np.sum(psf)
+    #test with no psf
+    #psf = np.zeros((dim_x,dim_y))
     return psf.reshape(psf.shape[0],psf.shape[1],1,1).astype('float32')
 
 
-def build_observation(true_image, true_psf):
-    ph_image = tf.placeholder(dtype=tf.float32, shape=true_image.shape)
-    ph_psf = tf.placeholder(dtype=tf.float32, shape=true_psf.shape)
+def build_observation(image, psf):
+    out = None #will be the convolution of the image and psf
 
-    out = None
+    #build the graph (use named scope to prevent any collisions)
+    with tf.variable_scope("observation"):
+        #inputs
+        ph_image = tf.placeholder(dtype=tf.float32, shape=image.shape)
+        ph_psf = tf.placeholder(dtype=tf.float32, shape=psf.shape)
 
-    with tf.Session() as sess:
         # convolve with the psf
         convolved_image = tf.nn.convolution(ph_image, ph_psf, "SAME")  # convolve(ph_image,ph_psf)
 
         # add poisson noise
         convolved_image += poisson_avg_value_per_photon * \
-                           tf.random_poisson(lam=poisson_avg_photons_per_pix, shape=true_image.shape, seed=SEED)
+                           tf.random_poisson(lam=poisson_avg_photons_per_pix, shape=image.shape, seed=SEED)
 
-        # run the graph
-        init = tf.global_variables_initializer()
-        sess.run(init)
-        out = sess.run(convolved_image, feed_dict={ph_image: true_image, ph_psf: true_psf})
+        with tf.Session() as sess:
+            # run the graph
+            sess.run(tf.global_variables_initializer())
+            out = sess.run(convolved_image, feed_dict={ph_image: image, ph_psf: psf})
 
-        #print(observation)
+            #print("Observation:\n ", out)
 
     return out
+
+
+def fit_model(observation,psf,iterations=100,learning_rate=0.5, l1_reg_scale=0.0001):
+    # step 5
+    best_model = None
+    with tf.variable_scope("fit"):
+        ph_obs = tf.placeholder(dtype=tf.float32, shape=observation.shape)
+        ph_psf = tf.placeholder(dtype=tf.float32, shape=psf.shape)
+        # ph_test = tf.placeholder(dtype=tf.float32, shape=observation.shape)
+
+        test_image, test_points = make_image(10, 10, 3)
+
+        var_model = tf.get_variable(name='model',
+                                    # shape=observation.shape,
+                                    dtype=tf.float32,
+                                    trainable=True,
+                                    constraint=lambda x: tf.clip_by_value(x, 0, np.inf),  # force positive only
+                                    initializer=build_observation(test_image, psf),
+                                    regularizer=tf.contrib.layers.l1_regularizer(scale=l1_reg_scale)  # todo: make scale tunable
+                                    )
+
+        #regularizer = tf.contrib.layers.l1_regularizer(scale=l1_reg_scale)
+       # weights = tf.trainable_variables()
+       # l1_reg_penalty = tf.contrib.layers.apply_regularization(tf.contrib.layers.l1_regularizer(scale=l1_reg_scale),weights)
+        loss = tf.nn.l2_loss(var_model - ph_obs) + sum(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)) # not adding a bias term (yet)
+        optimizer = tf.train.AdamOptimizer(learning_rate)
+        train = optimizer.minimize(loss)  # use defaults for now
+
+        # could have just done train = tf.train.AdamOptimizer(0.5).minimize(loss), but want to be more explicitly clear
+
+        with tf.Session() as sess:
+            # run the graph
+            sess.run(tf.global_variables_initializer())
+
+            #todo: make the iteration length variable (base on input size or stabilization of loss?)
+            for _ in range(iterations):
+                sess.run(train, feed_dict={ph_obs: observation, ph_psf: psf})
+
+                # see how we are progressing
+                print(sess.run(loss, feed_dict={ph_obs: observation, ph_psf: psf}))
+                # print(sess.graph.get_tensor_by_name('fit/model:0').eval())
+
+            best_model = sess.run(var_model)
+
+            print("test",best_model )
+
+    return best_model
+
+
+def chi_sqr(obs, exp, error=None):
+
+    obs = np.array(obs).flatten()
+    exp = np.array(exp).flatten()
+
+    if error is not None:
+        error = np.array(error)
+
+    #trim non-values
+    if 0:
+        remove = []
+        for i in range(len(obs)):
+            if obs[i] > 98.0:
+                remove.append(i)
+
+        obs = np.delete(obs, remove)
+        exp = np.delete(exp, remove)
+        if error is not None:
+            error = np.delete(error, remove)
+
+    if error is not None:
+        c = np.sum((obs*exp)/(error*error)) / np.sum((exp*exp)/(error*error))
+    else:
+        c = 1.0
+
+    chisqr = 0
+    if error is None:
+        error=np.zeros(len(obs))
+        error += 1.0
+
+    for i in range(len(obs)):
+        chisqr = chisqr + ((obs[i]-c*exp[i])**2)/(error[i])
+
+    return chisqr,c
+
+def fidelity(true_image,model_image):
+    #say, chi2 over all pixels??
+    assert true_image.shape == model_image.shape
+
+    chi2, c = chi_sqr(true_image,model_image)
+    return chi2
 
 def main():
 
     #start with the true data
-    true_image, true_points = make_true_image(10,10,3)
+    true_image, true_points = make_image(10,10,3)
     true_psf = make_psf(3,3)
 
-    #step 5
-    observation = build_observation(true_image,true_psf)
+    observation = build_observation(true_image, true_psf)
+    model = fit_model(observation,true_psf)
 
-    ph_obs = tf.placeholder(dtype=tf.float32, shape=observation.shape)
-    ph_psf = tf.placeholder(dtype=tf.float32, shape=true_psf.shape)
+    #todo: deconvolve the model (remove the psf) and compare to true_image
 
-    var_model = tf.get_variable(name='model',
-                                shape=observation.shape,
-                                dtype=tf.float32,
-                                trainable=True,
-                                constraint=lambda x: tf.clip_by_value(x, 0, np.inf), #force positive only
-                                initializer=None,
-                                regularizer= tf.contrib.layers.l1_regularizer(scale=1.0) #todo: make scale tunable
-                                )
+    print("true", observation)
 
+    print("\n\ndiff", observation-model)
 
-
-    with tf.Session() as sess:
-
-        #convolve with the true PSF
-        var_model = tf.nn.convolution(var_model, ph_psf, "SAME")
-
-        obj = tf.nn.l2_loss(ph_obs - var_model) #not adding a bias term (yet)
-        optimizer = tf.train.AdamOptimizer().minimize(obj) #use defaults for now
-
-
-
-        # run the graph
-        init = tf.global_variables_initializer()
-        sess.run(init)
-
-        out = sess.run(optimizer,feed_dict={ph_obs: observation,ph_psf:true_psf})
-
-
-
-        print(out,sess.graph.get_tensor_by_name('model:0').eval())
-
-
+    print(fidelity(observation,model))
 
    # print(observation)
 
